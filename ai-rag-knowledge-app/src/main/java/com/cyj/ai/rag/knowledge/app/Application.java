87b10f3222478d2fd52b49e138696962ae0e67cf
magicxiaobao <magicxiaobao@noreply.gitcode.com>
2025-03-13 23:41:52 +0800

    init project

---
+++ ai-rag-knowledge-app/src/main/java/com/cyj/ai/rag/knowledge/app/Application.java
@@ -0,0 +1,11 @@
+package com.cyj.ai.rag.knowledge.app;
+
+/**
+ * 功能描述:
+ *
+ * @author chenyuejun
+ * @version 1.0.0
+ * @since 2025.03.13
+ **/
+public class Application {
+}
---
+++ ai-rag-knowledge-app/src/main/java/com/cyj/ai/rag/knowledge/app/package-info.java
@@ -0,0 +1,1 @@
+package com.cyj.ai.rag.knowledge.app;
